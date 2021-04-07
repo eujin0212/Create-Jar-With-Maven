726c34440d9de7677c82345bd9fc7477be89d2c3
Eu Jin <61020650+eujin0212@users.noreply.github.com>
2021-04-07 12:48:36 +0800

    Issue#5

---
+++ HelloWorld/src/main/java/org/eujin/App.java
@@ -0,0 +1,20 @@
+package org.eujin;
+
+import javax.swing.*;
+
+public class App extends JFrame
+{
+    public App(){
+        setTitle("Hello World");
+        setSize(688,488);
+        setLayout(null);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(3);
+        setVisible(true);
+    }
+    public static void main( String[] args )
+    {
+        new App();
+
+    }
+}
