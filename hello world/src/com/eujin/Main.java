a9d8fd82d1554f9c9420bdc291a6972b140ed406
Eu Jin <61020650+eujin0212@users.noreply.github.com>
2021-04-07 12:47:20 +0800

    Issue#5

---
+++ hello world/src/com/eujin/Main.java
@@ -0,0 +1,10 @@
+package com.eujin;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello eujin!");
+        System.out.println("try me!");
+    }
+}
